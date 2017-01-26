a05b313fc93315881ed37eed5b8f93d81729ba24
PlugaruT <plugaru.tudor@gmail.com>
2017-01-26 14:19:06 +0200

    add posibility to fully edit users

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,7 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($mdDialog, Restangular) {
+  function MainController($mdDialog, $mdToast, Restangular) {
     var vm = this;
     activate();
 
@@ -17,27 +17,53 @@
     }
 
     vm.deleteUser = function (user) {
-      user.doDELETE();
+      user.doDELETE().then(function () {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent('User deleted successfully')
+            .position('bottom left')
+            .hideDelay(3000)
+        );
+      });
       _.remove(vm.userList, user)
     };
 
-    vm.showEditUserPrompt = function (ev, user) {
-      var confirm = $mdDialog.prompt()
-        .title('Change First Name')
-        .initialValue(user.first_name)
-        .targetEvent(ev)
-        .ok('Save')
-        .cancel('Cancel');
-
-      $mdDialog.show(confirm).then(function (result) {
-        user.first_name = result;
-        user.put();
-      });
+    vm.showEditUserPrompt = function (event, user) {
+      $mdDialog.show({
+        locals: {user: user},
+        controller: EditUserDialogController,
+        controllerAs: 'ctrl',
+        templateUrl: 'app/components/edit-user-dialog/edit-user-dialog.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: event,
+        clickOutsideToClose: true
+      })
     };
+
+    function EditUserDialogController($scope, $mdDialog, user) {
+      $scope.user = user;
+      $scope.createUser = function (user) {
+        user.put().then(function () {
+          $mdToast.show(
+            $mdToast.simple()
+              .textContent('User saved successfully')
+              .position('bottom left')
+              .hideDelay(3000)
+          );
+        });
+        $mdDialog.hide();
+      }
+    }
 
     vm.createUser = function () {
       var user = Restangular.restangularizeElement(undefined, vm.user, 'users');
       user.post().then(function (response) {
+        $mdToast.show(
+          $mdToast.simple()
+            .textContent('User created successfully')
+            .position('bottom left')
+            .hideDelay(3000)
+        );
         vm.userList.splice(0, 0, response)
       });
     };
